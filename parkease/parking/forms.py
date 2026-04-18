fc3af037eb0da8ac132f9b12c0d100d718fd9eb2
oscarh <oluyimbazi@student.refactory.academy>
2026-04-17 22:48:50 -0700

    changed timezone and added form validation

--- parkease/parking/forms.py
+++ parkease/parking/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from .models import Vehicle
+import re
 
 class VehicleForm(forms.ModelForm):
     class Meta:
@@ -29,9 +30,52 @@ class VehicleForm(forms.ModelForm):
             'model':{'required':'Please enter the model'},
             'color':{'required':'Please enter the color'},
             'phone_number':{'required':'Please enter your phone number'},
-            'nin':{'required':'Please enter your nin'},
-            'number_plate':{'required':'Please enter your number plate'}
+            'nin':{'required':'Please enter your nin','unique':
+            'NIN already exists, please enter unique nin'},
+            'number_plate':{'required':'Please enter your number plate',
+            'unique':'Number already exists, enter unique number plate'}
 
         }
+
+    def clean_driver_name(self):
+        name=self.cleaned_data['driver_name']
+        if len(name) < 3:
+            raise forms.ValidationError('Driver name must be atleast 3 characters')
+        if not name.replace(" ","").isalpha():
+            raise forms.ValidationError("Driver name must not contain numbers")
+        return name
+        
+    def clean_phone_number(self):
+        phone=self.cleaned_data['phone_number']
+        if not re.match(r'^07\d{8}$',phone):
+            raise forms.ValidationError('Enter a valid phone number e.g 0709135086')
+        return phone
+        
+    def clean_nin(self):
+        nin=self.cleaned_data['nin']
+
+        if len(nin) != 14:
+            raise forms.ValidationError('NIN must be 14 characters')
+            
+        return nin
+    
+    def clean_number(self):
+        plate=self.cleaned_data['number_plate'].upper()
+
+        # starting with U
+        if not plate.startswith('U'):
+            raise forms.ValidationError('Number plate must start U')
+        # must be alphanumeric 
+        if not plate.isalnum():
+            raise forms.ValidationError('Number plate must contain only letters and numbers.')
+        
+        # must be less than 6 characters 
+        if len(plate) >= 6:
+            raise forms.ValidationError('Number plate must be less than 6 characters.')
+        return plate
+            
+    
+        
+
 
     
