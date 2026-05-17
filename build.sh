7a281e974d28b2e030f9f9b41c9658a532ad0d11
luyimbazioscar <luyimbazioscar53@gmail.com>
2026-05-17 10:55:01 -0700

    fixed build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# 1. Install dependencies from the root directory
 pip install -r requirements.txt
 
-python manage.py collectstatic --noinput
+# 2. Change directory into your Django project folder where manage.py is located
+# (Change 'parkease' to 'parking' if that is your folder name)
+cd parkease
 
+# 3. Run management commands now that we are in the correct folder
+python manage.py collectstatic --noinput
 python manage.py migrate
